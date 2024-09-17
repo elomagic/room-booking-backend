be63065c5a8f14bcf0876a4ad963e5ef16c6abc5
C. Rambow <rambow.dev@elomagic.de>
2024-09-17 06:35:58 +0200

    Changed to record

--- src/main/java/de/elomagic/rb/backend/dtos/VersionDTO.java
+++ src/main/java/de/elomagic/rb/backend/dtos/VersionDTO.java
@@ -2,30 +2,9 @@ package de.elomagic.rb.backend.dtos;
 
 import jakarta.annotation.Nonnull;
 
-public class VersionDTO {
-
-    private String backendVersion;
-    private String timestamp;
-    private String frontendVersion;
-
-    public VersionDTO() {}
-
-    public VersionDTO(@Nonnull String backendversion, @Nonnull String timestamp, @Nonnull String frontendVersion) {
-        this.backendVersion = backendversion;
-        this.timestamp = timestamp;
-        this.frontendVersion = frontendVersion;
-    }
-
-    public String getBackendVersion() {
-        return backendVersion;
-    }
-
-    public String getTimestamp() {
-        return timestamp;
-    }
-
-    public String getFrontendVersion() {
-        return frontendVersion;
-    }
-
+public record VersionDTO(
+        @Nonnull String backendversion,
+        @Nonnull String timestamp,
+        @Nonnull String frontendVersion
+) {
 }
